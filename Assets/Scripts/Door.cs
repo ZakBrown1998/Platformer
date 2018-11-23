1a693381f37225b954a5e7118f86f481259aaad8
ZakBrown1998 <43067354+ZakBrown1998@users.noreply.github.com>
2018-11-23 14:51:41 +0000

    Added in scripts for pickups

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    //Variable to let us add to the score
+    //Public so we can drag and drop
+    public Score scoreObject;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreObject.SaveScore();
+	}
+}
