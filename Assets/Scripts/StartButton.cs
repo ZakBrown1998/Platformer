a035dc8d8afea302bb8fd848d1823714ded25efd
ZakBrown1998 <43067354+ZakBrown1998@users.noreply.github.com>
2018-11-30 14:28:26 +0000

    Added in Start Button and Title Button

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This allows us to use the scene loading function
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+   //This will be called by the Button component
+   //When the button is clicked
+    public void StartGame()
+    {
+        //Reset the Score
+        PlayerPrefs.DeleteKey("score");
+
+        SceneManager.LoadScene("Level 1");
+    }
+	
+}
